ebd6741b6f0d70af218b4e56f288a3f999251577
Derek Huang <derek.huang@monash.edu>
2022-09-27 16:38:55 +1000

    Added Lungs model

---
+++ Assets/Scripts/LungAniController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LungAniController : MonoBehaviour
+{
+    // references to each animator
+    public Animation diaphragmAni;
+    public Animation heartAni;
+    public Animation leftLungAni;
+    public Animation rightLungAni;
+    public Animation tracheaAni;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
